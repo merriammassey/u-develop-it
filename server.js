01b9b9fe7f6d439b42d6eebb95e9f61f847c8ae7
Maria M <prov21_1@hotmail.com>
2021-04-17 14:37:25 -0700

    add CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const express = require('express');
+const inputCheck = require('./utils/inputCheck.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
+// Create connection and store in db
 const db = mysql.createConnection(
     {
       host: 'localhost',
@@ -20,6 +21,7 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
+  //http get request 
 app.get('/', (req, res) => {
     res.json({
       message: 'Hello World'
@@ -30,34 +32,90 @@ app.get('/', (req, res) => {
     console.log(rows);
   });
 
-  //query to return a single candidate
-  db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
+  // Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
       if (err) {
-          console.log(err);
+        res.status(400).json({ error: err.message });
+        return;
       }
-      console.log(row);
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
   });
 
-  // Delete a candidate
-/* db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result);
-  }); */
 
+// Delete a candidate using api endpoint
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+}); 
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    //database call
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+  }
+  res.json({
+    message: 'success',
+    data: body
+  });
+});
+  });
 
   // Create a candidate
 const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
 VALUES (?,?,?,?)`;
 const params = [1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params, (err, result) => {
-if (err) {
-console.log(err);
-}
-console.log(result);
-});
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
   //error reporting
   // Default response for any other request (Not Found)
