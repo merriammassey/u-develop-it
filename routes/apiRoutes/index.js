7ae3d30d493504d6417a8c87bcf121fe45fef64f
Maria M <prov21_1@hotmail.com>
2021-04-17 19:55:15 -0700

    vote routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+router.use(require('./voteRoutes'));
 router.use(require('./voterRoutes'));
 router.use(require('./partyRoutes'));
 //const apiRoutes = require('./routes/apiRoutes/');
