e462a51f77ac74a1614b8d8db0ea9bfb1659b92a
Maria M <prov21_1@hotmail.com>
2021-04-17 19:32:48 -0700

    voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-router.use(require('./voterRoutes'));
 const router = express.Router();
+router.use(require('./voterRoutes'));
 router.use(require('./partyRoutes'));
-const apiRoutes = require('./routes/apiRoutes');
-
-
+//const apiRoutes = require('./routes/apiRoutes/');
 router.use(require('./candidateRoutes'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes/');
 const db = require('./db/connections');
 
 const PORT = process.env.PORT || 3001;
